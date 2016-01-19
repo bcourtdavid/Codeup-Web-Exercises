4c43cc650f2feff2a0bf67264a9b9ac26451fe00
David Betancourt <d.a.betancourt@gmail.com>
2016-01-18 19:43:15 -0600

    if else js practice

--- public/js/if_else.js
+++ public/js/if_else.js
@@ -6,19 +6,20 @@ var color = colors[Math.floor(Math.random()*colors.length)];
 var favorite = 'Blue'; // TODO: change this to your favorite color from the list
 if(color == "Red") {
 	console.log("Red is the color of blood.");
-} else if(color == "Orange") {
+} else if(color === "Orange") {
 	console.log(color + " is the color of a basketball.");
-} else if(color == "Yellow") {
+} else if(color === "Yellow") {
 	console.log(color + " is the color of the sun.");
-} else if(color == "Green") {
+} else if(color === "Green") {
 	console.log(color + " is the color of money.");
-} else if(color == "Blue") {
+} else if(color === "Blue") {
 	console.log(color + " is the color of the sky.");
 } else {
 	console.log("I do not know anything by that color.");
 }
 
-(color == favorite) ? console.log(color + " is my favorite color.") : console.log(color + " is not my favorite color.");
+(color == favorite) ? console.log(color + " is my favorite color.") : 
+	console.log(color + " is not my favorite color.");
 // TODO: Create a block of if/else statements to check for every color except indigo and violet.
 // TODO: When a color is encountered log a message that tells the color, and an object of that color.
 //       Example: Blue is the color of the sky.
