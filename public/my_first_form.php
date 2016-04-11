a4209606ad5a1b0dc6acda19d46bf85ca19ae8a9
David Betancourt <d.a.betancourt@gmail.com>
2016-04-11 17:54:13 -0500

    added other form types and attribute

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -9,8 +9,9 @@
     <meta charset = "utf-8">
 </head>
 <body>	
-    <!-- This exercise pracitces the use of form attributes and identifies why the use of 
-         a button tag is preferrable over an "input type = 'submit'" -->
+    <!-- This exercise pracitces the use of some of the different types of form inputs that are available, 
+         and how forms are submitted, we can start to talk about individual input types. -->
+    <h2>User Login</h2>
 	<form method="POST" action="/my_first_form.php">
     <p>
         <label for="username">Username</label>
@@ -18,10 +19,28 @@
     </p>
     <p>
         <label for="password">Password</label>
-        <input id="password" name="password" type="password" placeholder="Enter Password">
+        <input id="password" name="password" type="hidden" placeholder="Enter Password">
+    </p>
+
+    <h2>Compose an Email</h2>
+    <form method="POST" action="/my_first_form.php">
+    <p>
+        <label for="recipient">To:</label>
+        <input id="recipient" name="recipient" type="text" placeholder="Send to">
     </p>
     <p>
-        <button type="submit">Login</button>
+        <label for="sender">From:</label>
+        <input id="sender" name="sender" type="text" placeholder="Sent from">
+    </p>
+    <p>
+        <label for="subject">Subject:</label>
+        <input id="subject" name="subject" type="text">
+    </p>
+    <p>
+        <textarea id="email_body" name="email_body" rows="5" cols="40">Content Here</textarea>
+    </p>
+    <p>
+        <button type="send">Send</button>
     </p>
 </form>
 </body>
